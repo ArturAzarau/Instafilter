3f25318f4489e48d3f1a873eb3ec3b774062adfe
ArturAzarau <artur.azarau@gmail.com>
2018-08-05 19:52:49 +0300

    Added functionality for importing pictures

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -15,11 +15,17 @@ final class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
     
+    // MARK: - Properties
+    
+    private var currentImage: UIImage!
+    
     // MARK: - View life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Adding bar button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
 
     // MARK: - Actions
@@ -35,6 +41,23 @@ final class ViewController: UIViewController {
     // MARK: -
     
     @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+    // MARK: - Methods
+    
+    @objc private func importPicture() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker, animated: true)
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
     }
 }
 
